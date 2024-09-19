c9de7f54eb4808de73ce7babb81bc4ff9e3a8a6f
Mili Virani <milivirani.dev@gmail.com>
2024-09-19 20:00:54 +0530

    Design Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./globals.css";
 
 interface Joke {
   id: number;
@@ -19,12 +20,33 @@ export default function Home() {
   };
 
   const [joke, setJoke] = useState<Joke | null>(null);
-
+  const [showPunchline, setShowPunchline] = useState(false);
+  useEffect(() => {
+    getJoke();
+  }, []);
   return (
-    <>
-      <p>{joke?.setup}</p>
-      <p>{joke?.punchline}</p>
-      <button onClick={getJoke}>One More</button>
-    </>
+    <div className="flex justify-around items-center h-screen">
+      <div>
+        <p>{joke?.setup}</p>
+        <div
+          onMouseEnter={() => setShowPunchline(true)}
+          onMouseLeave={() => setShowPunchline(false)}
+          onClick={() => setShowPunchline(!showPunchline)}
+        >
+          {showPunchline ? (
+            <p className="no-underline">{joke?.punchline}</p>
+          ) : (
+            <p className="underline">Tap or hover to view</p>
+          )}
+        </div>
+        <button
+          type="button"
+          onClick={getJoke}
+          className="rounded-md bg-green-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-green-700 focus:shadow-none active:bg-green-800 hover:bg-green-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:bg-green-400 disabled:shadow-none ml-2"
+        >
+          One More
+        </button>
+      </div>
+    </div>
   );
 }
